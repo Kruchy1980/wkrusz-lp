a27add789b77454443b0e5cdb6b7c32f88fc9477
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-04-27 22:46:33 +0200

    About styles added

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
+(() => {
+    const roller = document.querySelector('.about-content__title');
+    const sectionHeight = document.querySelector('.about-content__text');
+    const dots = document.querySelector('.multi-dot');
+
+    const showHidden = () => {
+        sectionHeight.classList.toggle('about-content__text--open');
+        // dots.classList.toggle('hide-dots');
+        if (dots.innerHTML === '<img class="about-content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dots.innerHTML = `<img class="about-content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dots.innerHTML = '<img class="about-content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    roller.addEventListener('click', showHidden);
+})();
 // Menu on button
 (() => {
     const button = document.querySelector('.js-main-navigation__inner__mobile-button');
