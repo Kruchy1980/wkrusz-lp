da5683f8ad63449ffaa7e86e21b18a82efcd6ab5
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-04-28 17:52:01 +0200

    7 bokmarks added to offer plus logic connected

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,10 @@
 
     const showHidden = () => {
         sectionHeightHistory.classList.toggle('about-content__text__history--open');
-        if (dotsHistory.innerHTML === '<img class="about-content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
-            dotsHistory.innerHTML = `<img class="about-content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        if (dotsHistory.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsHistory.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
         } else {
-            dotsHistory.innerHTML = '<img class="about-content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+            dotsHistory.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
         }
         if (sectionHeightHistory.classList.contains('active')) {
             sectionHeightHistory.classList.toggle('active');
@@ -26,13 +26,142 @@
 
     const showHidden = () => {
         sectionHeightDescription.classList.toggle('about-content__text__description--open');
-        if (dotsDescription.innerHTML === '<img class="about-content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
-            dotsDescription.innerHTML = `<img class="about-content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        if (dotsDescription.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsDescription.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
         } else {
-            dotsDescription.innerHTML = '<img class="about-content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+            dotsDescription.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
         }
     }
     rollerDescription.addEventListener('click', showHidden);
+})();
+// OFFER
+// 1.  Biznes Plan, wniosek kredytowy, analiza opłacalności investycji
+(() => {
+    const rollerBusiness = document.querySelector('.offer-content__title__business');
+    const sectionHeightBusiness = document.querySelector('.offer-content__text__business');
+    const dotsBusiness = document.querySelector('.multi-dot__business');
+
+    const showHidden = () => {
+        sectionHeightBusiness.classList.toggle('offer-content__text__business--open');
+        if (dotsBusiness.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsBusiness.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsBusiness.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerBusiness.addEventListener('click', showHidden);
+})();
+// 2. Calculation
+(() => {
+    const rollerCalculation = document.querySelector('.offer-content__title__calculation');
+    const sectionHeightCalculation = document.querySelector('.offer-content__text__calculation');
+    const dotsCalculation = document.querySelector('.multi-dot__calculation');
+
+    const showHidden = () => {
+        sectionHeightCalculation.classList.toggle('offer-content__text__calculation--open');
+        if (dotsCalculation.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculation.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculation.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculation.addEventListener('click', showHidden);
+})();
+// 3. Calculation test
+(() => {
+    const rollerCalculationMark = document.querySelector('.offer-content__title__calculation__mark');
+    const sectionHeightCalculationMark = document.querySelector('.offer-content__text__calculation__mark');
+    const dotsCalculationMark = document.querySelector('.multi-dot__calculation__mark');
+
+    const showHidden = () => {
+        sectionHeightCalculationMark.classList.toggle('offer-content__text__calculation__mark--open');
+        if (dotsCalculationMark.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculationMark.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculationMark.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculationMark.addEventListener('click', showHidden);
+})();
+// 4. Calculation test
+(() => {
+    const rollerCalculationTest = document.querySelector('.offer-content__title__calculation__test');
+    const sectionHeightCalculationTest = document.querySelector('.offer-content__text__calculation__test');
+    const dotsCalculationTest = document.querySelector('.multi-dot__calculation__test');
+
+    const showHidden = () => {
+        sectionHeightCalculationTest.classList.toggle('offer-content__text__calculation__test--open');
+        if (dotsCalculationTest.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculationTest.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculationTest.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculationTest.addEventListener('click', showHidden);
+})();
+// 5. Calculation test
+(() => {
+    const rollerCalculationInvestor = document.querySelector('.offer-content__title__calculation__investor');
+    const sectionHeightCalculationInvestor = document.querySelector('.offer-content__text__calculation__investor');
+    const dotsCalculationInvestor = document.querySelector('.multi-dot__calculation__investor');
+
+    const showHidden = () => {
+        sectionHeightCalculationInvestor.classList.toggle('offer-content__text__calculation__investor--open');
+        if (dotsCalculationInvestor.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculationInvestor.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculationInvestor.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculationInvestor.addEventListener('click', showHidden);
+})();
+// 6. Due-diligence
+(() => {
+    const rollerCalculationDueDiligence = document.querySelector('.offer-content__title__calculation__due-diligence');
+    const sectionHeightCalculationDueDiligence = document.querySelector('.offer-content__text__calculation__due-diligence');
+    const dotsCalculationDueDiligence = document.querySelector('.multi-dot__calculation__due-diligence');
+
+    const showHidden = () => {
+        sectionHeightCalculationDueDiligence.classList.toggle('offer-content__text__calculation__due-diligence--open');
+        if (dotsCalculationDueDiligence.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculationDueDiligence.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculationDueDiligence.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculationDueDiligence.addEventListener('click', showHidden);
+})();
+// 7. Transfers
+(() => {
+    const rollerCalculationTransfers = document.querySelector('.offer-content__title__calculation__transfers');
+    const sectionHeightCalculationTransfers = document.querySelector('.offer-content__text__calculation__transfers');
+    const dotsCalculationTransfers = document.querySelector('.multi-dot__calculation__transfers');
+
+    const showHidden = () => {
+        sectionHeightCalculationTransfers.classList.toggle('offer-content__text__calculation__transfers--open');
+        if (dotsCalculationTransfers.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculationTransfers.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculationTransfers.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculationTransfers.addEventListener('click', showHidden);
+})();
+// 8. Systems
+(() => {
+    const rollerCalculationSystems = document.querySelector('.offer-content__title__calculation__systems');
+    const sectionHeightCalculationSystems = document.querySelector('.offer-content__text__calculation__systems');
+    const dotsCalculationSystems = document.querySelector('.multi-dot__calculation__systems');
+
+    const showHidden = () => {
+        sectionHeightCalculationSystems.classList.toggle('offer-content__text__calculation__systems--open');
+        if (dotsCalculationSystems.innerHTML === '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">') {
+            dotsCalculationSystems.innerHTML = `<img class="content__title__arrow-icon" src="./images/arrow-icon/2.arrow-up.png" alt="Arrow Up">`
+        } else {
+            dotsCalculationSystems.innerHTML = '<img class="content__title__arrow-icon" src="./images/arrow-icon/1.arrow-down.png" alt="Arrow Down">'
+        }
+    }
+    rollerCalculationSystems.addEventListener('click', showHidden);
 })();
 // Menu on button
 (() => {
